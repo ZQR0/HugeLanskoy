676b2024a0b39e469c74568bb2562791676940e9
ZQR0 <yarikrech@gmail.com>
2022-10-08 16:43:16 +0300

    added spawn command

---
+++ src/main/java/me/zqr0/hugelanskoy/Commands/SpawnLanskoy.java
@@ -0,0 +1,37 @@
+package me.zqr0.hugelanskoy.Commands;
+
+import me.zqr0.hugelanskoy.Lanskoy.Manager;
+import me.zqr0.hugelanskoy.Plugin;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+import org.bukkit.entity.*;
+
+public class SpawnLanskoy implements CommandExecutor {
+
+    private final Manager manager;
+
+    public SpawnLanskoy(Plugin thisPlugin) {
+        manager = new Manager(thisPlugin);
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+
+        Player player = (Player) sender;
+        boolean isAggressive = true;
+
+        try {
+            manager.spawnLanskoy(player.getLocation(), isAggressive);
+            player.sendMessage(ChatColor.GREEN + "Lanskoy на свободе!");
+            return true;
+        } catch (NullPointerException ex) {
+            player.sendMessage(ChatColor.RED + "Error in this command");
+            ex.printStackTrace();
+            return false;
+        }
+    }
+}
