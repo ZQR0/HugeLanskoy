732e037164e9834ba9931171944b27bde580fd11
ZQR0 <yarikrech@gmail.com>
2022-10-08 16:45:28 +0300

    lanskoy manager file to use it in events

---
+++ src/main/java/me/zqr0/hugelanskoy/Lanskoy/Manager.java
@@ -0,0 +1,21 @@
+package me.zqr0.hugelanskoy.Lanskoy;
+
+import me.zqr0.hugelanskoy.Events.LanskoyEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+
+
+public class Manager {
+
+
+    public Manager(Plugin thisPlugin) {
+        Bukkit.getPluginManager().registerEvents(new LanskoyEvent(this), thisPlugin);
+    }
+
+    public void spawnLanskoy(@NotNull Location playerLocation, @NotNull boolean isAggressive) {
+        new Lanskoy(playerLocation, isAggressive).startLanskoy();
+    }
+
+}
