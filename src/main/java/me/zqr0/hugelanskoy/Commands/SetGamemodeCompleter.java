7d3e12393b57f7a3369d1d85f31793fad01d2471
ZQR0 <yarikrech@gmail.com>
2022-10-09 09:46:57 +0300

    added completer for setgamemode command

---
+++ src/main/java/me/zqr0/hugelanskoy/Commands/SetGamemodeCompleter.java
@@ -0,0 +1,21 @@
+package me.zqr0.hugelanskoy.Commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class SetGamemodeCompleter implements TabCompleter {
+    @Nullable
+    @Override
+    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (args.length != 0) {
+            return List.of("creative", "survival");
+        }
+
+        return null;
+    }
+}
