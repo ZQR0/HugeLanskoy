6d44c59d9c34614149055ffe1135dced3b49eeba
ZQR0 <yarikrech@gmail.com>
2022-10-14 17:52:43 +0300

    added damage event method

--- src/main/java/me/zqr0/hugelanskoy/Events/LanskoyEvent.java
+++ src/main/java/me/zqr0/hugelanskoy/Events/LanskoyEvent.java
@@ -1,11 +1,16 @@
 package me.zqr0.hugelanskoy.Events;
 
 import me.zqr0.hugelanskoy.Lanskoy.Manager;
+import org.bukkit.entity.Giant;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.event.player.PlayerCommandSendEvent;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class LanskoyEvent implements Listener {
 
@@ -17,13 +22,31 @@ public class LanskoyEvent implements Listener {
 
     @EventHandler
     public void startLanskoy(EntitySpawnEvent event, PlayerCommandSendEvent playerEvent) {
-        Player player = (Player) playerEvent.getPlayer();
+        Player player = playerEvent.getPlayer();
         boolean isAggressive = true;
 
         try {
             manager.spawnLanskoy(player.getLocation(), isAggressive);
         } catch (NullPointerException ex) {
-            event.setCancelled(true);
+            ex.printStackTrace();
+        }
+    }
+
+    @EventHandler
+    public void onDamageGiant(EntityDamageEvent event) {
+        try {
+            List<EntityDamageEvent.DamageCause> damageCauses = Arrays.asList(
+                    EntityDamageEvent.DamageCause.FIRE,
+                    EntityDamageEvent.DamageCause.FALL,
+                    EntityDamageEvent.DamageCause.ENTITY_EXPLOSION,
+                    EntityDamageEvent.DamageCause.BLOCK_EXPLOSION,
+                    EntityDamageEvent.DamageCause.FIRE_TICK
+            );
+
+            if ((damageCauses.contains(event.getCause())) && (event.getEntity() instanceof Giant)) {
+                event.setCancelled(true);
+            }
+        } catch (NullPointerException | IndexOutOfBoundsException ex) {
             ex.printStackTrace();
         }
     }
